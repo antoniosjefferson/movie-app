e42cc0c70479b9897df62f94816df4393feeeae3
antoniosjefferson <itsmehtone@gmail.com>
2022-07-06 22:09:23 -0700

    Updates homework for 6/5

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,12 @@ class ActorsController < ApplicationController
     actor = Actor.find_by(id: params["id"])
     render json: actor.as_json
   end
+
+  def actor
+    render json: params[:name]
+  end
+
+  def info
+    render json: params[:name]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # Here I was not sure how to do the exercise with query, url, and body params
+
   get "/single_actor", controller: "actors_controller", action: "single_actor"
 
   get "/all_movies", controller: "movies", action: "all_movies"
+
+  get "/actor" => "actors#actor"
+
+  get "/actor/genre/:year" => "actors#info"
 end
